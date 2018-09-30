a422447057542bf6c9837ed08997ae4ba4e3dc1e
Victor Li <victorli5674@yahoo.com>
2018-09-29 22:58:00 -0700

    generate keys and installed afnetworking library

---
+++ PhotoMap/KeysSample.swift
@@ -0,0 +1,16 @@
+//
+//  KeysSample.swift
+//  PhotoMap
+//
+//  Created by Victor Li on 9/29/18.
+//  Copyright © 2018 Victor Li. All rights reserved.
+//
+
+import Foundation
+
+// create a new file called Keys.swift and copy this file
+// make sure to change struct name to Keys with your real API keys
+struct KeysSample {
+    static let FOURSQUARE_CLIENT_ID = ""
+    static let FOURSQUARE_CLIENT_SECRET = ""
+}
