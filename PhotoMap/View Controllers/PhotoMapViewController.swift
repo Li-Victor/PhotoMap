fe68e734a0d3bc6dfdbed405bb9ce4280e452f33
Victor Li <victorli5674@yahoo.com>
2018-09-30 13:11:13 -0700

    User can take a photo

--- PhotoMap/View Controllers/PhotoMapViewController.swift
+++ PhotoMap/View Controllers/PhotoMapViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    private var image: UIImage?
+    
+    @IBAction func didTapCamera(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        image = (info[UIImagePickerControllerOriginalImage] as! UIImage)
+        
+        // Dismiss UIImagePickerController
+        dismiss(animated: true, completion: nil)
+        self.performSegue(withIdentifier: "tagSegue", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // set map to sf region
         let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
         mapView.setRegion(sfRegion, animated: false)
         // Do any additional setup after loading the view.
